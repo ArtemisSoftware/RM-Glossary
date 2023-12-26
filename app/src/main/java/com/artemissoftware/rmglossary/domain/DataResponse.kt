d0810e6c98eb75f3cd72c79c410627f24fa18bbc
gustavo <gustavo.isaac@gmail.com>
2023-12-26 15:23:13 +0000

    Data response success and failure

--- app/src/main/java/com/artemissoftware/rmglossary/domain/DataResponse.kt
+++ app/src/main/java/com/artemissoftware/rmglossary/domain/DataResponse.kt
@@ -3,4 +3,14 @@ package com.artemissoftware.rmglossary.domain
 sealed interface DataResponse<T> {
     data class Success<T>(val data: T) : DataResponse<T>
     data class Failure<T>(val exception: Exception) : DataResponse<T>
+
+    fun onSuccess(block: (T) -> Unit): DataResponse<T> {
+        if (this is Success) block(data)
+        return this
+    }
+
+    fun onFailure(block: (Exception) -> Unit): DataResponse<T> {
+        if (this is Failure) block(exception)
+        return this
+    }
 }
